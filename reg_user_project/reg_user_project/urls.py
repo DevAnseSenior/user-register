cbdf705f0b731f4e39781fb8847111a3c8fe5030
Anderson Coelho <anderson.coelho1969@gmail.com>
2023-09-27 21:41:22 -0400

    initial commit

---
+++ reg_user_project/reg_user_project/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from app_reg_users import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+]
