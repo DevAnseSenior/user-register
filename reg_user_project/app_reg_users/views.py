8c378e6fcf2e76526a7dc7681c20adf0398db9f0
Anderson Coelho <anderson.coelho1969@gmail.com>
2023-10-01 14:04:17 -0400

    Refact project structure

--- reg_user_project/app_reg_users/views.py
+++ reg_user_project/app_reg_users/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from .models import User
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'users/home.html')
-
-def users(request):
-    new_user = User()
-    new_user.name = request.POST.get('name')
-    new_user.age = request.POST.get('age')
-    new_user.save()
-
     users = {
         'users': User.objects.all()
     }
+    return render(request, 'users/home.html', users)
 
-    return render(request, 'users/users.html', users)
+
+def create_user(request):
+    if request.method == 'POST':
+        new_user = User()
+        new_user.name = request.POST.get('name')
+        new_user.age = request.POST.get('age')
+
+        if not User.objects.filter(name=new_user.name):
+            new_user.save()
+        else:
+            print('user exists')
+
+
+    return render(request, 'users/create_user.html')
--- reg_user_project/reg_user_project/urls.py
+++ reg_user_project/reg_user_project/urls.py
@@ -5,5 +5,5 @@ from app_reg_users import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('users/', views.users, name='list_users'),
+    path('users/', views.create_user, name='create_user'),
 ]
